f664fd053e17ee10b155aa59f923fbf80a3ce13d
Vlad1kent1 <karabin0347@gmail.com>
2026-04-15 14:43:36 +0200

    ci: final fix knip

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,14 +1,33 @@
-export { Button, AnimatedButton } from './button'
-export { Calendar } from './calendar'
-export { DatePicker } from './date-picker'
-export { DecorativeBox } from './decorative-box'
-export { Field, FieldLabel, FieldDescription, FieldError, FieldGroup, FieldLegend, FieldSeparator, FieldSet, FieldContent, FieldTitle,} from './field'
-export { Input } from './input'
-export { Text } from './text'
-export { Popover, PopoverContent, PopoverTrigger } from './popover'
-export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select'
-export { Separator } from './separator'
-export { Toaster } from './sonner'
-export { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs'
-export { Textarea } from './textarea'
-export { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from './tooltip'
+export { Button, AnimatedButton } from './button';
+export { Calendar } from './calendar';
+export { DatePicker } from './date-picker';
+export { DecorativeBox } from './decorative-box';
+export {
+  Field,
+  FieldLabel,
+  FieldDescription,
+  FieldGroup,
+  FieldLegend,
+  FieldSet,
+  FieldContent,
+} from './field';
+export { Input } from './input';
+export { Text } from './text';
+export { Popover, PopoverContent, PopoverTrigger } from './popover';
+export {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './select';
+export { Separator } from './separator';
+export { Toaster } from './sonner';
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs';
+export { Textarea } from './textarea';
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from './tooltip';
